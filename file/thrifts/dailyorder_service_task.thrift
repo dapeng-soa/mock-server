4ec4f3397b0f318970c5d393972fed003cc296ff
leihuazhe <295482300@qq.com>
2018-11-08 18:52:40 +0800

    add parse metadata

--- file/thrifts/dailyorder_service_task.thrift
+++
@@ -1,9 +0,0 @@
-namespace java com.today.api.dailyorder.service
-
-/**
-* 定时任务
-**/
-service DailyOrderScheduledService {
-
-    void autoCalculateDailyReport()
-}
--- file/thrifts/order_query_service_request.thrift
+++
@@ -1,6 +0,0 @@
-namespace java com.today.api.order.vo
-
-struct TQueryOrderPaymentRequest {
-    1: string orderNo
-    2: i32 paySeq
-}
